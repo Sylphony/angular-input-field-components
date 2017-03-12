3ec16f22daddf23b230e015d0cb42ef23be38f2d
Sylphony <symphonictwist@gmail.com>
2017-03-11 17:20:27 -0800

    Added more tests for input text

--- src/field.component.spec.js
+++ src/field.component.spec.js
@@ -23,7 +23,9 @@ describe("Field Components", function() {
         $compile = _$compile_;
     }));
 
-    // Text field tests
+    /*
+     * Text field tests
+     */
     describe("<field type='text'>", function() {
         let component;
 
@@ -42,18 +44,64 @@ describe("Field Components", function() {
             );
         });
 
-        it("should contain a <label> immediately as the child", () => {
+        it("should contain only one child element and it is <label>", () => {
             const labelEle = component.children();
 
+            expect(labelEle.length).toBe(1);
             expect(labelEle[0].nodeName).toBe("LABEL");
         });
 
+        it("should contain only one element for title and it is <span>", () => {
+            const titleEle = component.find("span");
+
+            expect(titleEle.length).toBe(1);
+        });
+
         it("should contain only one input element and it is <input type='text'>", () => {
-            const inputEle = component.find('input');
+            const inputEle = component.find("input");
 
             expect(inputEle.length).toBe(1);
             expect(inputEle[0].type).toBe("text");
         });
-    });
 
+        describe("<label>", function() {
+            it("should contain all its expected attributes", () => {
+                const labelEle = component.children();
+
+                expect(labelEle[0].hasAttribute("class")).toBe(true);
+                expect(labelEle[0].hasAttribute("ng-class")).toBe(true);
+            });
+        });
+
+        describe("<span> title", function() {
+            it("should contain all its expected attributes", () => {
+                const titleEle = component.find("span");
+
+                expect(titleEle[0].hasAttribute("class")).toBe(true);
+                expect(titleEle[0].hasAttribute("ng-class")).toBe(true);
+            });
+        });
+
+        describe("<input type='text'>", function() {
+            it("should contain all its expected attributes", () => {
+                const inputEle = component.find("input");
+
+                expect(inputEle[0].hasAttribute("type")).toBe(true);
+                expect(inputEle[0].hasAttribute("class")).toBe(true);
+                expect(inputEle[0].hasAttribute("placeholder")).toBe(true);
+                expect(inputEle[0].hasAttribute("value")).toBe(true);
+                expect(inputEle[0].hasAttribute("maxlength")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-class")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-model")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-click")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-focus")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-blur")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-keypress")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-keydown")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-keyup")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-mouseover")).toBe(true);
+                expect(inputEle[0].hasAttribute("ng-disabled")).toBe(true);
+            });
+        });
+    });
 });
